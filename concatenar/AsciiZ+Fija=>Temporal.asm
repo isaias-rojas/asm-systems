7f04f67ddaf9b3851cdd0dc908145e1406aa3a92
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2024-02-25 01:35:37 -0400

    conectar asciiz - Dei

---
+++ concatenar/AsciiZ+Fija=>Temporal.asm
@@ -0,0 +1,41 @@
+; asciiz + Fija => Temporal
+
+lea si, cadena1
+lea bx, cadena2
+lea di, cadena3
+mov cx, 3
+inc di
+xor dx, dx
+
+ciclo1:
+  cmp [si], 0
+  je ciclo2
+  mov al, [si]
+  mov [di], al
+  inc si
+  inc di
+  inc dl
+  jmp ciclo1
+
+
+ciclo2:
+  cmp cl, 0
+  je terminar
+  mov al, [bx]
+  mov [di], al
+  inc bx
+  inc di
+  dec cl
+  inc dl
+  jmp ciclo2
+
+terminar:
+  lea di, cadena3
+  mov [di], dl
+  ret
+
+
+cadena1 db "abc",0
+cadena2 db "123"
+
+cadena3 db 100 dup("?")
